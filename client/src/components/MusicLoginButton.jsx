6e6c8118ed94ee06e57e634c61e9d1ad62feb136
Nick Havens <npahavens@gmail.com>
2017-10-12 15:26:38 -0500

    display warning if no active device is found after Spotify login

--- client/src/components/MusicLoginButton.jsx
+++ client/src/components/MusicLoginButton.jsx
@@ -1,8 +1,12 @@
 var MusicLoginButton = (props) => {
-
   return (
+    <div>
+    {!props.deviceFound && props.loggedIn &&
+      <div className='deviceWarning'>No active devices found for current user. Make sure the Spotify app is open on at least 1 device
+      </div>}
     <div className='musicButton' onClick={props.handleClick}>
         <div className='musicBtnText'>Log into Spotify to activate player</div>
+    </div>
     </div>
   )
 }
