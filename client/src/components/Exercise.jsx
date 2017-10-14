39fec6f2eb77974be18b94256d30ff7add48720b
Joey Li <joeyzaozaoli@gmail.com>
2017-10-13 21:39:48 -0700

    Change Exercise component from stateless to stateful and style favorite button on whether it is favorite exercise or not

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,15 +1,26 @@
-var Exercise = (props) => (
-  <div className="exercise">
-    <div className="exerciseDescription">
-      <img className="exerciseImage" src={props.exercise.picture} />
-      <p>
-        <span className="exerciseName">{props.exercise.name}</span>
-        <button onClick={ () => {props.favorite(props.exercise);} }>favorite</button>
-      </p>
-      {props.exercise.description}
-    </div>
-  </div>
-);
+class Exercise extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    var style = {
+      backgroundColor: this.props.userFavorites.includes(this.props.exercise.name) ? 'red' : null
+    };
+
+    return(
+      <div className="exercise">
+        <div className="exerciseDescription">
+          <img className="exerciseImage" src={this.props.exercise.picture} />
+          <p>
+            <span className="exerciseName">{this.props.exercise.name}</span>
+            <button style={style} onClick={ () => {this.props.favorite(this.props.exercise);} }>favorite</button>
+          </p>
+          {this.props.exercise.description}
+        </div>
+      </div>
+    );
+  }
+}
 
 window.Exercise = Exercise;
