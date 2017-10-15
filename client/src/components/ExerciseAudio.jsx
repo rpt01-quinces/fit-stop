4b2670bdee38aea71423e79009f07329798eb879
Nick Havens <npahavens@gmail.com>
2017-10-14 21:45:20 -0500

    basic toggle button working for exercise audio

--- client/src/components/ExerciseAudio.jsx
+++ client/src/components/ExerciseAudio.jsx
@@ -1,11 +1,17 @@
 const ExerciseAudio = (props) => {
+  console.log(props.muted)
   return (
     <div className='exerciseAudio'>
-      <div className='exerciseAudioBtn'>Exercise Audio</div>
-      <audio
-        autoPlay
+      <div
+        className='exerciseAudioBtn'
+        onClick={props.onToggle}
+      >Exercise Audio</div>
+      {!props.muted && <audio
+        autoPlay={!props.muted}
         src={props.source}
       ></audio>
+      }
+
     </div>
     )
 }
