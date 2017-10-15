b75cf2931f7422627f3ce08e4872fbf80caf672d
Joey Li <joeyzaozaoli@gmail.com>
2017-10-15 10:04:56 -0700

    Edit notice rendered on History component

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -9,14 +9,19 @@ class History extends React.Component {
   render() {
     return (
       <div className="history">
-        <h2 className='historyHeader'> History </h2>
-        <h2 className='favoritesHeader' onMouseOver={this.handleMouseOver.bind(this)} onMouseLeave={this.handleMouseLeave.bind(this)}> Favorites </h2>
+        {this.props.loggedIn && (<h2 className='historyHeader'>History</h2>)}
+        {this.props.loggedIn && (<h2 className='favoritesHeader' onMouseOver={this.handleMouseOver.bind(this)} onMouseLeave={this.handleMouseLeave.bind(this)}>Favorites</h2>)}
 
         {this.props.loggedIn && this.state.showHistory && this.props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
 
         {this.props.loggedIn && !this.state.showHistory && (<p>{this.props.userFavorites}</p>)}
 
-        {!this.props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
+        {!this.props.loggedIn && (
+          <div className='noticeNotLoggedIn'>
+            <p>You are currently not logged in.</p>
+            <p>Please Log In or Sign Up to view Workout History and Favorited Exercises.</p>
+          </div>
+        )}
       </div>
     );
   }
