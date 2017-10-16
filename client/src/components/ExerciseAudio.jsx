8f3d24b94d71c535c6c8c3aa47b75b81443db6c3
Nick Havens <npahavens@gmail.com>
2017-10-16 11:45:02 -0500

    exerciseAudioButton turns bule if active and white if muted

--- client/src/components/ExerciseAudio.jsx
+++ client/src/components/ExerciseAudio.jsx
@@ -1,9 +1,16 @@
 const ExerciseAudio = (props) => {
+
+  let style = {
+    'background-color': props.muted ? 'white' : '#3b8fb5',
+    'color': props.muted ? '#3b8fb5' : 'white'
+  };
+
   return (
     <div className='exerciseAudio'>
       <div
         className='exerciseAudioBtn'
         onClick={props.onToggle}
+        style={style}
       >Exercise Audio:{props.muted ? ' Off' : ' On'}</div>
       {!props.muted &&
         <audio
